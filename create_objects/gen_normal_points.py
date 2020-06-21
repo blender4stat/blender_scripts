cea35e8f07047ee244c0b5a71c4c1c30c802d96f
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-22 00:55:45 +0800

    Create gen_normal_points.py
    Generate the points on the density function.

---
+++ create_objects/gen_normal_points.py
@@ -0,0 +1,25 @@
+# Generate the points on the density function.
+
+from scipy.stats import norm
+import numpy as np
+
+n = 100
+scale_x = 3
+scale_z = 12
+
+x = numpy.linspace(-9, 9, n)
+z = scale_z * norm.pdf(x / scale_x)
+
+import bpy
+
+mesh        = bpy.data.meshes.new("newmesh")
+object      = bpy.data.objects.new(mesh.name, mesh)
+collection  = bpy.data.collections.get("Collection")
+collection.objects.link(object)
+bpy.context.view_layer.objects.active = object
+
+vertices_all = list(zip(x, [0] * n, z))
+edges_all = []
+faces_all = []
+
+mesh.from_pydata(vertices_all, edges_all, faces_all)
