2563904555f8cf8e0d590ad371736f78a6f42cee
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-22 21:01:43 +0800

    Update gen_normal_points.py
    1) Create a curve by adding edges
    2) Fix a typo

--- create_objects/gen_normal_points.py
+++ create_objects/gen_normal_points.py
@@ -7,7 +7,7 @@ n = 100
 scale_x = 3
 scale_z = 12
 
-x = numpy.linspace(-9, 9, n)
+x = np.linspace(-9, 9, n)
 z = scale_z * norm.pdf(x / scale_x)
 
 import bpy
@@ -19,7 +19,7 @@ collection.objects.link(object)
 bpy.context.view_layer.objects.active = object
 
 vertices_all = list(zip(x, [0] * n, z))
-edges_all = []
+edges_all = list(zip(range(0, n - 1), range(1, n)))
 faces_all = []
 
 mesh.from_pydata(vertices_all, edges_all, faces_all)
